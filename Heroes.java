1e9d9382da234b526b94406b28ad464121340a65
Karlan <zngr96@yadnex.ru>
2024-02-14 21:53:32 +0300

    create hero sniper

---
+++ Heroes.java
@@ -0,0 +1,14 @@
+abstract class Heroes {
+    protected String name;
+    protected int hp;
+    protected int stamina;
+    protected int agility;
+
+    protected int intellect;
+
+    protected int strange;
+
+
+
+
+}
